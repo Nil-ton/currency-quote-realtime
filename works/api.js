a81716245d1b2656f413ba050f3f7690aac0ae6c
unknown <nilton.oliveira0305@gmail.com>
2023-08-23 03:50:50 -0300

    correção na data

--- works/api.js
+++ works/api.js
@@ -15,9 +15,8 @@ const month = [
 
 function generateDate() {
   const date = new Date();
-  const day = date.getDay().toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
   const monthIndex = date.getMonth();
-  const year = date.getFullYear();
   const hour = date.getHours().toString().padStart(2, "0");
   const min = date.getMinutes().toString().padStart(2, "0");
 
@@ -26,9 +25,6 @@ function generateDate() {
 
 function generateHoursAndSeconds() {
   const date = new Date();
-  const day = date.getDay().toString().padStart(2, "0");
-  const monthIndex = date.getMonth();
-  const year = date.getFullYear();
   const hour = date.getHours().toString().padStart(2, "0");
   const min = date.getMinutes().toString().padStart(2, "0");
   const seconds = date.getSeconds().toString().padStart(2, "0");
